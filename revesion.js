51547db53ea643333a9970a5663bddec1c63cdc6
GIT INDIA <gitechno.in@gmail.com>
2025-08-02 14:43:55 +0530

    Add files via upload

---
+++ revesion.js
@@ -0,0 +1,54 @@
+document.write('eee');
+
+// var 
+
+// var a = 56;
+
+// document.write("\n",typeof(a));
+// document.write("\n",a);
+// var a = 67;
+// document.write("\n",a);
+
+// a = 98;
+// document.write("\n",a);
+
+
+// let 
+
+// let a = 56;
+
+// document.write("\n",typeof(a));
+// document.write("\n",a);
+// // let a = 67;
+// document.write("\n",a);
+
+// a = 98;
+// document.write("\n",a)
+
+
+// const a = 56;
+
+// document.write("\n",typeof(a));
+// document.write("\n",a);
+// // let a = 67;
+// document.write("\n",a);
+
+// a = 98;
+// document.write("\n",a);
+
+
+
+// javascript hoisting meaning
+
+// console.log(a);
+
+// var a;
+
+
+// Uncaught ReferenceError: Cannot access 'a' before initialization
+// console.log(a);
+// let a;
+
+
+// console.log(a);
+// const a='';
